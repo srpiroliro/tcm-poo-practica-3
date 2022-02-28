80834f86a63ffd3953dc3fc337fed43ae2d61117
PolRubio <apocrb23@gmail.com>
2022-02-28 20:47:41 +0100

    no acabat - fet fins totalPindolesQueden() - falta mètode ordenar()

--- Sessio1/Malalt.java
+++ Sessio1/Malalt.java
@@ -1,5 +1,60 @@
 package Sessio1;
 
 public class Malalt {
+	private String nom;
+	private MedicamentPindoles[] medicaments;
+	private int num;
+	private final int increment;
+	
+	public Malalt(String nom, int numMax, int increment) {
+		this.nom = nom;
+		medicaments = new MedicamentPindoles[numMax];
+		this.increment = increment;
+	}
 
+	public void comprarMedicamentPindoles(MedicamentPindoles p) { //crec que es podria millorar
+		if(num==medicaments.length) ampliar();
+		num++;
+		medicaments[num] = p;
+		ordenar();
+	}
+	public void comprarMedicamentPindoles(String p, int pindoles) {
+		MedicamentPindoles nouMedicament = new MedicamentPindoles(p, pindoles);
+		comprarMedicamentPindoles(nouMedicament);
+	}
+	
+	//GETs
+	public String getNom() {return nom;}
+	public int getNum() {return num;}
+	public int getIncrement() {return increment;}
+	public MedicamentPindoles getMedicamentPindoles(int quin) {
+		if(quin <= num) return medicaments[quin];
+		return null;
+	}
+	public MedicamentPindoles getMedicamentNoBuit() {
+		for(int i=0; i<=num; i++) {
+			if(medicaments[i].quantesUnitatsQueden()>0) return medicaments[i];
+		}
+		return null;
+	}
+	public int totalPindolesQueden() {
+		int n = 0;
+		for(int i=0; i<=num; i++) {
+			n += medicaments[i].quantesUnitatsQueden();
+		}
+		return n;
+	}
+	
+	//PRIVATEs
+	private void ampliar() {
+		MedicamentPindoles[] nousMedicaments = new MedicamentPindoles[medicaments.length + increment];
+		
+		for(int i = 0; i<this.medicaments.length; i++) {
+			nousMedicaments[i] = medicaments[i];
+		}
+		medicaments = nousMedicaments;
+	}
+	private void ordenar() { //falta fer
+		
+	}
 }
