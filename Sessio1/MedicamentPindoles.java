3b0ebcd32922e9fb41e4e00e6f21249861b7ac0a
PolRubio <apocrb23@gmail.com>
2022-02-28 19:22:27 +0100

    primera versió - en teoria acabat

--- Sessio1/MedicamentPindoles.java
+++ Sessio1/MedicamentPindoles.java
@@ -1,5 +1,35 @@
 package Sessio1;
 
 public class MedicamentPindoles {
-
+	private final String nom;
+	private final int unitats;
+	private int preses = 0;
+	
+	public MedicamentPindoles(String nom, int unitats){
+		this.nom = nom.substring(0,1).toUpperCase() + nom.substring(1).toLowerCase();
+		this.unitats = unitats;
+		
+	}
+	
+	//GETs
+	public int quantesUnitatsQueden() {return unitats-preses;}	
+	public void prendrePindola() {if(preses < unitats) preses++;}
+	public int getPindolesPreses() {return preses;}
+	public String getNom() {return nom;}
+	
+	//OVERRIDES
+	public boolean equals(Object o) {
+		MedicamentPindoles altreMedicament;
+		
+		if(o instanceof MedicamentPindoles) {
+			altreMedicament = (MedicamentPindoles) o;
+			if(nom == altreMedicament.getNom())
+				return true;
+		}
+		return false;
+	}
+	
+	public String toString() {
+		return "Nom del medicament " + nom + " amb " + quantesUnitatsQueden() + " píndoles de " + unitats;
+	}
 }
